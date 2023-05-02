f3f252fe6229f1adb192e1ee08e8dc272aa294a4
Dumani Mbili <dumanimbili@gmail.com>
2023-05-02 14:53:22 +0200

    included button, modified styling, simplified js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttons = document.querySelectorAll("button");
 const resultsDiv = document.querySelector("#results");
 const scoreDiv = document.querySelector("#score");
+const restartButton = document.querySelector("#restart");
 let playerScore = 0;
 let computerScore = 0;
 
@@ -11,41 +12,56 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  const options = ["rock", "paper", "scissors"];
+  const winningCombinations = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
+
   const playerSelectionLower = playerSelection.toLowerCase();
   const computerSelectionLower = computerSelection.toLowerCase();
-  if (
-    (playerSelectionLower === "rock" &&
-      computerSelectionLower === "scissors") ||
-    (playerSelectionLower === "paper" && computerSelectionLower === "rock") ||
-    (playerSelectionLower === "scissors" && computerSelectionLower === "paper")
-  ) {
+
+  if (!options.includes(playerSelectionLower)) {
+    return "invalid";
+  }
+
+  if (playerSelectionLower === computerSelectionLower) {
+    return "It's a tie! You both chose ${playerSelection}.";
+  }
+
+  if (winningCombinations[playerSelectionLower] === computerSelectionLower) {
     playerScore++;
     resultsDiv.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
-  } else if (
-    (playerSelectionLower === "rock" && computerSelectionLower === "paper") ||
-    (playerSelectionLower === "paper" &&
-      computerSelectionLower === "scissors") ||
-    (playerSelectionLower === "scissors" && computerSelectionLower === "rock")
-  ) {
+  } else {
     computerScore++;
     resultsDiv.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
-  } else {
-    resultsDiv.textContent = `It's a tie! You both chose ${playerSelection}.`;
   }
+
   scoreDiv.textContent = `Score: ${playerScore} - ${computerScore}`;
+
   if (playerScore === 5 || computerScore === 5) {
     if (playerScore > computerScore) {
       resultsDiv.textContent = `You win the game!`;
     } else {
       resultsDiv.textContent = `You lose the game!`;
     }
-    playerScore = 0;
-    computerScore = 0;
+    restartButton.style.display = "block";
   }
 }
+
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     playRound(button.id, computerPlay());
   });
 });
+
+restartButton.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  scoreDiv.textContent = "Score: 0 - 0";
+  resultsDiv.textContent = "";
+  restartButton.style.display = "none";
+});
+``;
